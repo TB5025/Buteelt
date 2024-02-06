ecea05c6488608d96a61d1999569930acf4d927e
“B210910036 <turb0ld9425@gmail.com>
2024-02-06 18:36:46 +0800

    hi

--- Buteelt/src/Main.java
+++ Buteelt/src/Main.java
@@ -31,11 +31,11 @@ public class Main {
     // 1. ArrayList_үүсгэх
     public static ArrayList<Integer> createList() {
         ArrayList<Integer> list = new ArrayList();
-        list.add(1);
-        list.add(2);
         list.add(3);
+        list.add(6);
+        list.add(7);
         list.add(8);
-        list.add(5);
+        list.add(9);
         return list;
     }
 
