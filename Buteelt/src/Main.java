e8d6d6aa42e1a12510c09c8464c537e3abf3682a
TB5025 <123079327+TB5025@users.noreply.github.com>
2024-02-06 18:54:48 +0800

    Update Main.java

--- Buteelt/src/Main.java
+++ Buteelt/src/Main.java
@@ -10,24 +10,19 @@ public class Main {
         int sum = calculateSum(myList);
         System.out.println("Элементүүдийн нийлбэр: " + sum);
 
-
         ArrayList<Integer> reversedList = sortDescList(myList);
         System.out.println("Элементүүдийг эрэмбэлэх: " + reversedList);
-
 
         int maxElement = findMaxElement(myList);
         System.out.println("Макс утга: " + maxElement);
 
-
         ArrayList<Integer> squaredList = squareElements(myList);
         System.out.println("Элементүүдийг квадрат: " + squaredList);
-
 
         ArrayList<Integer> evenNumbers = filterEvenNumbers(myList);
         System.out.println("Сондгой тоо: " + evenNumbers);
     }
 
-    // 1. ArrayList_үүсгэх
     public static ArrayList<Integer> createList() {
         ArrayList<Integer> list = new ArrayList();
         list.add(3);
@@ -38,7 +33,6 @@ public class Main {
         return list;
     }
 
-    // 2. Элементүүдий_нийлбэр_олгох
     public static int calculateSum(ArrayList<Integer> list) {
         int sum = 0;
         for (int num : list) {
@@ -47,14 +41,12 @@ public class Main {
         return sum;
     }
 
-    // 3. Элементүүдийг_эрэмбэлэх
     public static ArrayList<Integer> sortDescList(ArrayList<Integer> list) {
         ArrayList<Integer> sortedDesc = new ArrayList(list);
         Collections.sort(sortedDesc, Collections.reverseOrder());
         return sortedDesc;
     }
 
-    // 4. Макс_утга
     public static int findMaxElement(ArrayList<Integer> list) {
         int max = Integer.MIN_VALUE;
         for (int num : list) {
@@ -65,7 +57,6 @@ public class Main {
         return max;
     }
 
-    // 5. Элементүүдийг_квадрат
     public static ArrayList<Integer> squareElements(ArrayList<Integer> list) {
         ArrayList<Integer> squared = new ArrayList();
         for (int num : list) {
@@ -74,7 +65,6 @@ public class Main {
         return squared;
     }
 
-    // 6. Сондгой_тоо
     public static ArrayList<Integer> filterEvenNumbers(ArrayList<Integer> list) {
         ArrayList<Integer> evenNumbers = new ArrayList();
         for (int num : list) {
